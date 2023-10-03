d27f302667ccd841d506209323b08ad221e2af4d
Matthew Shuttleworth <M.P.Shuttleworth@leeds.ac.uk>
2023-10-03 11:24:20 +0100

    Overhaul

--- README.md
+++ README.md
@@ -5,11 +5,9 @@ Additional configs and scripts for the AMPI robotics manufacturing platform
 SSH into your pi and enter the following commands
 
 ```
-cd ~/printer_data/config
-git clone https://github.com/ShuttleworthM/RatOS-RoboMFG-Additions.git RoboMFG
-mv printer.cfg printer_OLD.cfg
-cp ~/printer_data/config/RoboMFG/template/robomfg-printer.template.cfg ~/printer_data/config/printer.cfg
-systemctl restart klipper
+cd ~/
+git clone https://github.com/Advanced-Manufacturing-at-UoL/Robot-MFG-Platform_RatOS-Additions.git
+bash ~/Robot-MFG-Platform_RatOS-Additions/install.sh
 ```
 
 # Update Manager
@@ -18,8 +16,9 @@ To recieve updates from the git repo add the following lines to the bottom of th
 ```
 [update_manager RoboMFG]
 type: git_repo
-path: ~/printer_data/config/RoboMFG
-origin: https://github.com/ShuttleworthM/RatOS-RoboMFG-Additions.git
 primary_branch: main
-is_system_service: False
+path: ~/Robot-MFG-Platform_RatOS-Additions
+origin: https://github.com/Advanced-Manufacturing-at-UoL/Robot-MFG-Platform_RatOS-Additions.git
+managed_services:
+	klipper
 ```
---
+++ install.sh
@@ -0,0 +1,85 @@
+# Prevent running as root.
+if [ ${UID} == 0 ]; then
+    echo -e "DO NOT RUN THIS SCRIPT AS 'root' !"
+    echo -e "If 'root' privileges needed, you will prompted for sudo password."
+    exit 1
+fi
+
+# Force script to exit if an error occurs
+set -e
+
+# Find SRCDIR from the pathname of this script
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/ && pwd )"
+
+# Default Parameters
+CONFIG_DIR="${HOME}/printer_data/config"
+
+function start_klipper {
+    sudo systemctl restart klipper
+}
+
+function stop_klipper {
+    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F "klipper.service")" ]; then
+        sudo systemctl stop klipper
+    else
+        echo "Klipper service not found, please install Klipper first"
+        exit 1
+    fi
+}
+
+function link_additions_folder {
+    if [ -d "${CONFIG_DIR}" ]; then
+        if [ -d "${CONFIG_DIR}/additions" ]; then
+            echo -e "${CONFIG_DIR}/custom already exists, skipping..."
+        else
+            ln -s ${SRCDIR}/additions ${CONFIG_DIR}/additions
+            echo -e "additions folder linked"
+        fi
+    else
+        echo -e "ERROR: ${CONFIG_DIR} not found."
+        exit 1
+    fi
+}
+
+function copy_board_files {
+    if [ -d "${CONFIG_DIR}" ]; then
+        sudo cp -av "${SRCDIR}/klipper_config/RatOS/boards/btt-ebb42-12b" "${CONFIG_DIR}/RatOS/boards"
+        echo -e "BTT EBB42 V1.2 board files copied"
+    else
+        echo -e "ERROR: ${CONFIG_DIR} not found."
+        exit 1
+    fi
+}
+
+function copy_cfg {
+    if [ -d "${CONFIG_DIR}" ]; then
+        mv printer.cfg printer_OLD.cfg
+        cp "${SRCDIR}/printer.cfg" "${CONFIG_DIR}/printer.cfg"
+        echo -e "printer.cfg copied"
+    else
+        echo -e "ERROR: ${CONFIG_DIR} not found."
+        exit 1
+    fi
+}
+
+function update_udev_rules {
+    if [ -d "${CONFIG_DIR}" ]; then
+        sudo ~/printer_data/config/RatOS/scripts/ratos-update.sh
+        echo -e "Udev rules updated"
+    else
+        echo -e "ERROR: ${CONFIG_DIR} not found."
+        exit 1
+    fi
+}
+
+echo -e "RoboMFG Additions"
+stop_klipper
+link_additions_folder
+copy_board_files
+copy_cfg
+update_udev_rules
+start_klipper
+echo -e ""
+echo -e "Installation finished!"
+echo -e ""
+exit 0
